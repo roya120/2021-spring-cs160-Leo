bc1fdeeafb1ecac604a8ecda12b63a6c97414b18
Roya Amjadi <60494742+roya120@users.noreply.github.com>
2021-03-27 11:46:50 -0700

    Delete src directory

--- src/Bookey.java
+++
@@ -1,8 +0,0 @@
-import java.sql.Connection;
-import java.sql.Statement;
-
-public class Bookey {
-	private static Connection con=null;
-	private static Statement stmt;
-	private static String url="jdbc:mysql://localhost:3306/ABCbank?serverTimezone=UTC";
-}
