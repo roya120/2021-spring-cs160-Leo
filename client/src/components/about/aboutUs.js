d0e0d7ef0cd8eb633945c397712772f12bb4a6e3
faribaasadi <60494710+faribaasadi@users.noreply.github.com>
2021-04-12 19:22:33 -0700

    Add files via upload

--- client/src/components/about/aboutUs.js
+++ client/src/components/about/aboutUs.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import about from '../images/about.jpg'
 
 
 function About() {
@@ -14,7 +15,16 @@ function About() {
         <a href="/about">About</a>
         </div>
             <div>
-                <h1>Coming Soon!!</h1>
+                <h3>About Us</h3>
+                <p>The physical weight of the book imparts a sense of gravitas.<br></br>
+                 In reading a book you are dealing with a real thing and not just digital wind,<br></br>
+                so it feels like something to take more seriously, respect more, and value greater than an ebook.<br></br><br></br>
+                Our goal is to bring a better experience of reading books and disconnect you from the digital world<br></br>
+                for moments. We collected books in different categories and you can borrow them and enjoy reading<br></br>
+                physical books with saving money and saving the planet.</p>
+                <div class= "aboutImage">
+                    <img src = {about} alt = "about" height="5%" width="50%"/>
+                </div>
                 
              
         </div>
