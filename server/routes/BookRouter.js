cbd2f22b08b630b85bc3ec44c8318f7f37d87d59
Sunny Xu <sunny.xu@sjsu.edu>
2021-04-25 22:21:41 -0700

    Delete BookRouter.js
    duplicate exists

--- server/routes/BookRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const BookController = require('../controllers/BookController')
-
-router.post('/book', BookController.insertBook)
-
-router.get('/book', BookController.searchBooks)
-
-module.exports = router
