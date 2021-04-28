90eed3f3f10005bf40f8753182d0a759c20c2723
faribaasadi <fariba.asadi@sjsu.edu>
2021-04-27 21:21:42 -0700

    updated client/server

---
+++ server/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: [true, "Please enter your name!"],
+        trim: true
+    },
+    lastname: {
+        type: String,
+        required: [true, "Please enter your name!"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email!"],
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password!"]
+    },
+    
+    
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Users", userSchema)
